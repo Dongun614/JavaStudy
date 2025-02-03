3b887cdfdd2dddf8d716879bea64053b112741da
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-02-03 23:18:15 +0900

    create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial{
+    public static void main(String[] args){
+        int values[] = {5, 10, 15};
+        for(int i: values)
+        System.out.println("Factorial of " + i + ":\t" + factorial(i));
+    }
+    private static long factorial(final int n){
+        long result = 1;
+        for(int i=1; i<=n; i++){
+            result *= i;
+        }
+        return result;
+    }
+}
