8d5f586bac85331cc8792dccd8448fd5727a7487
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-02-04 16:34:47 +0900

    create AutoBoxing.java

---
+++ AutoBoxing.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutoBoxing{
+    public static void main(String[] args){
+        List<Integer> IntegerList = new ArrayList<>();
+        for(int i = 1; i<10; i++){
+            integerList.add(i); //int to Integer; add(Integer.valueOf(i))
+        }
+        
+        System.out.println(intergerList);
+    }
+}
