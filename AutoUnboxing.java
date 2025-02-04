a4516cf48a091f25c80c5476c1fce0efa2f035f9
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-02-04 16:38:24 +0900

    create AutoUnboxing.java

---
+++ AutoUnboxing.java
@@ -0,0 +1,18 @@
+public class AutoUnboxing{
+    public static void main(String[] args){
+        List<Integer> integerList = new ArrayList<>();
+        for(int i=1; i<=5; i++){
+            integerList.add(i);
+        }
+        System.out.println(integerList);
+
+        int sumOfEven = 0;
+        for(integer i: integerList){
+            if(i % 2 == 0){
+                sumOfEven += i;
+            }
+        }
+
+        System.out.println(sumOfEven);
+    }
+}
